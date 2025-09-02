427cfd496649996c8a2f2e5848e18256ec9c2d94
Georges Huysmans <yo3dgj@hcms.ro>
2025-09-02 11:51:43 +0300

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -27,6 +27,7 @@ main() {
         run_with_log install_pyserial
         run_with_log enable_uart_and_serial0
         dialog --title "Reboot Required" --msgbox "✅ UART enabled and udev rule installed.\n\nSystem must reboot now." 12 60
+        clear
         sudo reboot
     else
         dialog --title "SA818 Skipped" --msgbox "⚠️ You chose not to configure SA818 hardware.\n\nSkipping UART and serial setup." 12 60
